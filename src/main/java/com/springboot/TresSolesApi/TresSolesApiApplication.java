4d765f4a3c0808db513b6e9906de3b1c8d0a55e8
Facundo Mansilla <fmansillla@gmail.com>
2022-05-15 16:49:43 -0300

    Primer commit

---
+++ src/main/java/com/springboot/TresSolesApi/TresSolesApiApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.TresSolesApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TresSolesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TresSolesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/TresSolesApi/TresSolesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.TresSolesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TresSolesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
